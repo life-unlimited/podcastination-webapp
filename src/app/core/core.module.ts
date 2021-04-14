8f5118140cff9446fe22b807b890970cfe3ffd3c
Lennart Altenhof <l.f.altenhof@gmail.com>
2021-04-14 20:48:08 +0200

    fix(webapp): some minor fixes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AngularMaterialModule } from '../utils/angular-material.module';
+import { AngularMaterialModule } from './utils/angular-material.module';
 
 
 @NgModule({
