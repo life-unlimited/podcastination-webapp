a5093ffa63719fd58c02aa49924ab5e81d423e90
Lennart Altenhof <l.f.altenhof@gmail.com>
2021-04-14 20:36:47 +0200

    feat(webapp): added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmbedPodcastComponent } from './features/embed-podcast/embed-podcast.component';
+import { EpisodeComponentComponent } from './features/embed-podcast/episode-component/episode-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmbedPodcastComponent,
+    EpisodeComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
