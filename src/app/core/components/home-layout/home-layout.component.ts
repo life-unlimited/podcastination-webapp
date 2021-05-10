27ebb0f81491f793057410404f58392fd1c53110
Lennart Altenhof <l.f.altenhof@gmail.com>
2021-05-10 16:07:25 +0200

    refactor: removed some unwanted code

--- src/app/core/components/home-layout/home-layout.component.ts
+++ src/app/core/components/home-layout/home-layout.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home-layout.component.html',
   styleUrls: ['./home-layout.component.css']
 })
-export class HomeLayoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HomeLayoutComponent {
 }
