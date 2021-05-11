2d0084941345f89993ed0f851f38678e9fdbd22b
Lennart Altenhof <l.f.altenhof@gmail.com>
2021-05-11 14:51:43 +0200

    test: fixed broken test for home layout component

--- src/app/core/components/home-layout/home-layout.component.spec.ts
+++ src/app/core/components/home-layout/home-layout.component.spec.ts
@@ -2,13 +2,15 @@ import { HomeLayoutComponent } from './home-layout.component';
 import { createRoutingFactory, SpectatorRouting } from '@ngneat/spectator';
 import { AppRoutes } from '../../constants/routes';
 import { CoreModule } from '../../core.module';
+import { ListenModule } from '../../../features/listen/listen.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('HomeLayoutComponent', () => {
   let spectator: SpectatorRouting<HomeLayoutComponent>;
   let component: HomeLayoutComponent;
   const createComponent = createRoutingFactory({
     component: HomeLayoutComponent,
-    imports: [CoreModule],
+    imports: [CoreModule, ListenModule, HttpClientTestingModule], // Import this stuff because of listen component being used on home.
     routes: AppRoutes,
     stubsEnabled: false
   });
