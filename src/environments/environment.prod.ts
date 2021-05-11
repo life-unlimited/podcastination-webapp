41bec366c949cf1b1cf4ae3284e6f83e937f7a9e
Lennart Altenhof <l.f.altenhof@gmail.com>
2021-05-11 10:54:14 +0200

    build: added version to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://podcast.lu-church.de/s',
-  staticUrl: 'https://podcast.lu-church.de/s/static'
+  staticUrl: 'https://podcast.lu-church.de/s/static',
+  version: require('../../package.json').version
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://podcast.lu-church.de/s',
-  staticUrl: 'https://podcast.lu-church.de/s/static'
+  staticUrl: 'https://podcast.lu-church.de/s/static',
+  version: require('../../package.json').version + '-dev'
 };
